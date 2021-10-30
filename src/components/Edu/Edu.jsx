e4173ed2154d6bef8579ae2cb56834377bcb9ba1
Kien <nkien5770@gmail.com>
2021-10-30 16:21:17 +0700

    -update

--- src/components/Edu/Edu.jsx
+++ src/components/Edu/Edu.jsx
@@ -4,41 +4,20 @@ import SchoolIcon from '@mui/icons-material/School'
 import BeatLoader from 'react-spinners/BeatLoader'
 
 export default function Edu() {
-  const [loading, setLoading] = useState(false)
-  useEffect(() => {
-    setLoading(true)
-    const timeLoad = setTimeout(() => {
-      setLoading(false)
-    }, 300)
-    return () => clearTimeout(timeLoad)
-  }, [])
   return (
     <S.Container>
-      {loading ? (
-        <BeatLoader
-          loading={loading}
-          size={30}
-          color={'#F4E84D'}
-          css={'margin-top:50px'}
-        />
-      ) : (
-        <>
-          <S.TitleHeader>EDUCATION</S.TitleHeader>
-          <S.Wrapper>
-            <S.Circle>
-              <SchoolIcon style={{ fontSize: '10rem' }} />
-            </S.Circle>
-            <S.ContentContainer>
-              <S.Content>
-                <p>
-                  I’m currently a Senior of Nguyen Tat Thanh University (NTTU)
-                </p>
-              </S.Content>
-              <S.Status>Status: Undergraduate</S.Status>
-            </S.ContentContainer>
-          </S.Wrapper>
-        </>
-      )}
+      <S.TitleHeader>EDUCATION</S.TitleHeader>
+      <S.Wrapper>
+        <S.Circle>
+          <SchoolIcon style={{ fontSize: '10rem' }} />
+        </S.Circle>
+        <S.ContentContainer>
+          <S.Content>
+            <p>I’m currently a Senior of Nguyen Tat Thanh University (NTTU)</p>
+          </S.Content>
+          <S.Status>Status: Undergraduate</S.Status>
+        </S.ContentContainer>
+      </S.Wrapper>
     </S.Container>
   )
 }
--- src/components/Exp/Exp.jsx
+++ src/components/Exp/Exp.jsx
@@ -3,49 +3,29 @@ import * as S from './exp.styled'
 import BeatLoader from 'react-spinners/BeatLoader'
 
 export default function Exp() {
-  const [loading, setLoading] = useState(false)
-  useEffect(() => {
-    setLoading(true)
-    const timeLoad = setTimeout(() => {
-      setLoading(false)
-    }, 300)
-    return () => clearTimeout(timeLoad)
-  }, [])
   return (
     <S.Container>
-      {loading ? (
-        <BeatLoader
-          loading={loading}
-          size={30}
-          color={'#F4E84D'}
-          css={'margin-top:50px'}
-        />
-      ) : (
-        <>
-          <S.TitleHeader>EXPERIENCE</S.TitleHeader>
-          <S.Wrapper>
-            <S.ContentContainer>
-              <S.ContentTitle>BEA. SHOP Project</S.ContentTitle>
-              <S.Desc>
-                <span>Project Description:</span> Create an e-commerce website,
-                which users can select and order their favorite clothing. Of
-                course, It’s just a demo. And I built it to apply for which I
-                learned.
-              </S.Desc>
-              <S.Desc>
-                <span>Team:</span> Only me
-              </S.Desc>
-              <S.Desc>
-                <span>Technology:</span> Frontend: ReactJS + Redux, Webpack.
-                Backend: NodeJs, MongoDB, Firebase
-              </S.Desc>
-              <S.Circle>
-                <S.Time>9/2021 - 11/2021</S.Time>
-              </S.Circle>
-            </S.ContentContainer>
-          </S.Wrapper>
-        </>
-      )}
+      <S.TitleHeader>EXPERIENCE</S.TitleHeader>
+      <S.Wrapper>
+        <S.ContentContainer>
+          <S.ContentTitle>BEA. SHOP Project</S.ContentTitle>
+          <S.Desc>
+            <span>Project Description:</span> Create an e-commerce website,
+            which users can select and order their favorite clothing. Of course,
+            It’s just a demo. And I built it to apply for which I learned.
+          </S.Desc>
+          <S.Desc>
+            <span>Team:</span> Only me
+          </S.Desc>
+          <S.Desc>
+            <span>Technology:</span> Frontend: ReactJS + Redux, Webpack.
+            Backend: NodeJs, MongoDB, Firebase
+          </S.Desc>
+          <S.Circle>
+            <S.Time>9/2021 - 11/2021</S.Time>
+          </S.Circle>
+        </S.ContentContainer>
+      </S.Wrapper>
     </S.Container>
   )
 }
