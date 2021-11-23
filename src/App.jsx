0cd642658ff9f14d5e916da3c2632f5b96b17b7f
Kien <nkien5770@gmail.com>
2021-11-23 16:28:48 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'normalize-css'
 import './assets/styles/global.scss'
 import Routes from './Routes'
+import styled,  {ThemeProvider} from "styled-components"
+import {lightTheme, darkTheme} from "./themes"
 
+const StyledApp = styled.div`
+
+`
 function App() {
+  const [theme, setTheme] = useState('light')
+  const themeToggler = () =>
+    setTheme(theme === 'light') ? setTheme('dark') : setTheme('light')
   return (
-    <div className="App">
-      <Routes />
-    </div>
-  )
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+
+      <div className="App">
+        <Routes />
+      </div>
+    )
+    </ThemeProvider>
 }
 
 export default App
